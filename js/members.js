31ac90e4cdabc851a3f9869092e47c7c18a5e3a7
楠見　朋也 <kusutomo143@gmail.com>
2019-06-27 19:35:15 +0900

    to high performance

--- js/members.js
+++ js/members.js
@@ -1,6 +1,1 @@
-$(function(){
-  $('.member-intro').fadeIn(2000);
-  $('#ogaki').fadeIn(2000);
-  $('.waves').fadeIn(2100);
-  $('.waves2').fadeIn(2100);
-});
+$(function(){$(".member-intro").fadeIn(2e3),$("#ogaki").fadeIn(2e3),$(".waves").fadeIn(2100),$(".waves2").fadeIn(2100)});
